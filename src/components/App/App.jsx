76218c37a5f5cc4af83dfb5a0f4acb6306552cfb
Tetiana Zykova <tatiana.nv96@gmail.com>
2021-12-24 01:23:51 +0200

    del api request

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,30 +9,6 @@ import s from './App.module.css';
 const App = () => {
   const contacts = useSelector(state => state.contacts.items);
 
-  // useEffect(() => {
-  //   const getContacts = async () => {
-  //     const data = await getAllContacts();
-  //     console.log('🚀 ~ data', data);
-  //     setstate(data);
-  //   };
-
-  //   const saveC = async data => {
-  //     const addD = await saveItem(data);
-  //     console.log('🚀 ~ addD', addD);
-  //   };
-
-  //   const delItem = async id => {
-  //     const datDel = await deleteItem(id);
-  //     console.log('🚀 ~ datDel', datDel);
-  //   };
-
-  //   saveC({ name: 'Mambo TI', number: '457733737' });
-  //   delItem(4);
-  //   getContacts();
-
-  //   return () => {};
-  // }, []);
-
   return (
     <div className={s.app}>
       <h1>Phonebook</h1>
